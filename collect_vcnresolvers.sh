88809cd08a1a33be1e063c8bafa86013936c3576
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-09-06 11:32:23 +0200

    Update collect_vcnresolvers.sh

--- collect_vcnresolvers.sh
+++ collect_vcnresolvers.sh
@@ -1,26 +1,24 @@
+
 #!/bin/sh
 export region=$1
+export tenancyname=$(oci iam compartment get --compartment-id $OCI_TENANCY | jq -r '.data | ."name"')
 
-rm -f vcnlist-$region.log
-rm -f vcnresolverslist-$region.log
-rm -f vcnresolvers-$region.log
-rm -f vcnresolvers-sh-$region.log
+rm -f vcnresolverslist-$region-$tenancyname.log
+rm -f vcnresolvers-$region-$tenancyname.log
+rm -f vcnresolver-sh-$region-$tenancyname.csv
+rm -f vcnresolvers_allregions_$tenancyname.csv
 
-oci search resource structured-search --query-text "query vcn resources" --region $region >> vcnlist-$region.log
-vcnlistcur=$(cat vcnlist-$region.log | jq .data |  jq .items | jq -r '.[] | ."identifier"')
+oci search resource structured-search --query-text "query dnsresolver resources" --region $region >> vcnresolverslist-$region-$tenancyname.log
+vcnresolverslistcur=$(cat vcnresolverslist-$region-$tenancyname.log | jq .data |  jq .items | jq -r '.[] | ."identifier"')
 
-for vcn in $vcnlistcur; do oci network vcn-dns-resolver-association get --vcn-id $vcn --region $region --query "data.[\"dns-resolver-id\"]" | jq -r '.[]' >> vcnresolverslist-$region.log ; done
-vcnresolverslistcur=$(cat vcnresolverslist-$region.log)
-for resolver in $vcnresolverslistcur; do oci dns resolver get --resolver-id $resolver --region $region >> vcnresolvers-$region.log ; done
+for resolver in $vcnresolverslistcur; do oci dns resolver get --resolver-id $resolver --region $region >> vcnresolvers-$region-$tenancyname.log ; done
 
-echo "[."display-name", ."id", ."endpoints"[]."is-listening", ."endpoints"[]."is-forwarding", ."endpoints"[]."listening-address", ."endpoints"[]."forwarding-address", ."rules"[]."destination-addresses"[], ."rules"[]."qname-cover-conditions"[] ]" >> vcnresolver-sh-$region.log
-cat vcnresolvers-$region.log | jq -r '.data | [ ."display-name", ."id", ."endpoints"[]."is-listening", ."endpoints"[]."is-forwarding", ."endpoints"[]."listening-address", ."endpoints"[]."forwarding-address", ."rules"[]."destination-addresses"[], ."rules"[]."qname-cover-conditions"[] ]  | @csv ' >> vcnresolver-sh-$region.log
-cat vcnresolver-sh-$region.log >> vcnresolvers_allregions.log
+echo "[."display-name", ."id", ."endpoints"[]."is-listening", ."endpoints"[]."is-forwarding", ."endpoints"[]."listening-address", ."endpoints"[]."forwarding-address", ."rules"[]."destination-addresses"[], ."rules"[]."qname-cover-conditions"[] ]" >> vcnresolver-sh-$region-$tenancyname.csv
+cat vcnresolvers-$region-$tenancyname.log | jq -r '.data | [ ."display-name", ."id", ."endpoints"[]."is-listening", ."endpoints"[]."is-forwarding", ."endpoints"[]."listening-address", ."endpoints"[]."forwarding-address", ."rules"[]."destination-addresses"[], ."rules"[]."qname-cover-conditions"[] ]  | @csv ' >> vcnresolver-sh-$region-$tenancyname.csv
+cat vcnresolver-sh-$region-$tenancyname.csv >> vcnresolvers_allregions_$tenancyname.csv
 
 export dir=$(pwd)
-echo $dir/vcnresolvers_allregions.log
+echo $dir/vcnresolvers_allregions_$tenancyname.csv
 
-rm -f vcnlist-$region.log
-rm -f vcnresolverslist-$region.log
-#rm -f vcnresolvers-$region.log
-rm -f vcnresolver-sh-$region.log
+rm -f vcnresolverslist-$region-$tenancyname.log
+rm -f vcnresolver-sh-$region-$tenancyname.csv
