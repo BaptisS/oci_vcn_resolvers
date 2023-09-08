079355fb34f741070cf533db862a7cde10b87333
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-09-08 12:00:29 +0200

    Create updatefwrules.sh

---
+++ updatefwrules.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+export res_region=$1
+export res_ocid=$2
+export res_excl=$3
+export fw_domains=$4 
+export fw_target=$5
+
+rm-f $res_ocid.logfile
+if [[ "$res_ocid" != "$res_excl" ]]; then
+export fw_ep_name=$(oci dns resolver get --region $res_region --resolver-id $res_ocid | jq -r '.data.endpoints[] | ."name"')
+export fw_rules='[{"action":"FORWARD","qnameCoverConditions":['$fw_domains'],"destinationAddresses":["'$fw_target'"],"sourceEndpointName":"'$fw_ep_name'"}]'
+oci dns resolver update --region $res_region--resolver-id $res_ocid --rules $fw_rules --force >> $res_ocid.logfile
+echo done
+fi
+
