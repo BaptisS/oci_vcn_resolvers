24b87ea94ed7c31abea71c44ac1ea7591437b944
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-09-12 10:48:42 +0200

    Update updatefwrules.sh

--- updatefwrules.sh
+++ updatefwrules.sh
@@ -9,7 +9,9 @@ rm -f $res_ocid.logfile
 if [[ "$res_ocid" != "$res_excl" ]]; then
 export fw_ep_name=$(oci dns resolver get --region $res_region --resolver-id $res_ocid --query 'data.endpoints[?("is-forwarding")]' | jq -r '.[] | ."name"')
 export fw_rules='[{"action":"FORWARD","qnameCoverConditions":['$fw_domains'],"destinationAddresses":["'$fw_target'"],"sourceEndpointName":"'$fw_ep_name'"}]'
-oci dns resolver update --region $res_region --resolver-id $res_ocid --rules $fw_rules --force >> $res_ocid.logfile
-echo done
+resupdate=$(oci dns resolver update --region $res_region --resolver-id $res_ocid --rules $fw_rules --force) 
+echo $resupdate >> $res_ocid.logfile 
+resupdatedmns=$(echo $resupdate | jq -r '.data.rules[] | ."qname-cover-conditions" | @csv')
+echo DNS Forwarding Rule updated : $resupdatedmns with Target : $fw_target  
 fi
 
