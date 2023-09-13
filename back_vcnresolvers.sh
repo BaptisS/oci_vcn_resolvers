1b44ef95fcc3d8487ec1182396b2680ddaaa625c
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-09-13 16:37:17 +0200

    Update back_vcnresolvers.sh

--- back_vcnresolvers.sh
+++ back_vcnresolvers.sh
@@ -3,7 +3,7 @@ export region=$1
 export tenancyname=$2
 oci search resource structured-search --query-text "query dnsresolver resources" --region $region >> resolverslist-$region.log
 resolverslistcur=$(cat resolverslist-$region.log | jq .data |  jq .items | jq -r '.[] | ."identifier"')
-for resolver in $resolverslistcur; do oci dns resolver get --resolver-id $resolver --region $region > $resolver.logfile && resname=$(cat $resolver.logfile | jq -r '.data | ."display-name"') && echo Backing up vcn resolver : $res_name ; done
+for resolver in $resolverslistcur; do oci dns resolver get --resolver-id $resolver --region $region > $resolver.logfile && res_name=$(cat $resolver.logfile | jq -r '.data | ."display-name"') && echo Backing up vcn resolver : $res_name ; done
 zip $tenancyname_$region_vcnresolvers_backup.zip *.logfile
 rm -f *.logfile
 dir=$(pwd)
