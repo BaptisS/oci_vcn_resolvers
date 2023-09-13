9864b1ca771d61f6ecf409634a009022f35056cc
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-09-13 19:13:11 +0200

    Create rest_vcnresolver.sh

---
+++ rest_vcnresolver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export filename=$1.logfile 
+export resocid=$1
+IFS="." tokens=( ${filename} )
+export region=$(echo ${tokens[3]})
+cat $filename | jq -r '.data | ."rules"' > $filename.rules.file
+sed -i 's/client-address-conditions/clientAddressConditions/g' $filename.rules.file
+sed -i 's/destination-addresses/destinationAddresses/g' $filename.rules.file
+sed -i 's/qname-cover-conditions/qnameCoverConditions/g' $filename.rules.file
+sed -i 's/source-endpoint-name/sourceEndpointName/g' $filename.rules.file
+sed -i 's/source-endpoint-name/sourceEndpointName/g' $filename.rules.file
+
+oci dns resolver update --region $region --resolver-id $resocid --rules file://$filename.rules.file
