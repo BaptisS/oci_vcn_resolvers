9799b06b075fba82850edc667d66ba37e2922049
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-09-14 15:44:02 +0200

    Update rest_vcnresolver.sh

--- rest_vcnresolver.sh
+++ rest_vcnresolver.sh
@@ -11,5 +11,6 @@ sed -i 's/destination-addresses/destinationAddresses/g' $filename.rules.file
 sed -i 's/qname-cover-conditions/qnameCoverConditions/g' $filename.rules.file
 sed -i 's/source-endpoint-name/sourceEndpointName/g' $filename.rules.file
 sed -i 's/source-endpoint-name/sourceEndpointName/g' $filename.rules.file
-
-oci dns resolver update --region $region --resolver-id $resocid --rules file://$filename.rules.file --force >> $region-vcnresolverupdate.log
+cat $filename | jq -r '.data | ."attached-views"' > $filename.attachedviews.file
+sed -i 's/view-id/viewId/g' $filename.attachedviews.file
+oci dns resolver update --region $region --resolver-id $resocid --attached-views file://$filename.attachedviews.file --rules file://$filename.rules.file --force >> $region-vcnresolverupdate.log
