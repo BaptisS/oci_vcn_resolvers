d880c990aabf6daea8bbe0b41a19e2600cd5a9d4
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-09-14 14:52:49 +0200

    Update rest_vcnresolver.sh

--- rest_vcnresolver.sh
+++ rest_vcnresolver.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export filename=$1.logfile 
 export resocid=$1
+saved_IFS=$IFS;
 IFS="." tokens=( ${filename} )
 export region=$(echo ${tokens[3]})
+IFS=$saved
 cat $filename | jq -r '.data | ."rules"' > $filename.rules.file
 sed -i 's/client-address-conditions/clientAddressConditions/g' $filename.rules.file
 sed -i 's/destination-addresses/destinationAddresses/g' $filename.rules.file
