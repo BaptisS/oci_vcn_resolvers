5dd5cd75f0498df37ceb9d84e88f3d25814bcb04
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-09-14 11:38:11 +0200

    Update prep-rest-vcnresolver.sh

--- prep-rest-vcnresolver.sh
+++ prep-rest-vcnresolver.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #prep-rest-vcnresolver.sh
-export dir=$1
+export regfile=$1
 export pathname=$2
-mkdir $pathname/$region
-mkdir $pathname/$region/bak
-mv $pathname/$region.zip $pathname/$region/
-unzip $pathname/$region/$region.zip -d unzip $pathname/$region/
-mv $pathname/$region/$region.zip $pathname/$region/bak/
+mkdir $pathname/$regfile
+mkdir $pathname/$regfile/bak
+mv $pathname/$regfile.zip $pathname/$regfile/
+unzip $pathname/$regfile/$regfile.zip -d unzip $pathname/$regfile/
+mv $pathname/$regfile/$regfile.zip $pathname/$regfile/bak/
 
-cd $pathname/$region/
+cd $pathname/$regfile/
 
 rm -f rest_vcnresolver.sh
 wget https://raw.githubusercontent.com/BaptisS/oci_vcn_resolvers/main/rest_vcnresolver.sh
@@ -17,7 +17,7 @@ chmod +x rest_vcnresolver.sh
 ls *.logfile > resolversfiles.list
 sed -i 's/.logfile//g' resolversfiles.list
 reslist=$(cat resolversfiles.list)
-for resolver in $reslist; do echo Restore VCN Resolver : $resolver && ./rest_vcnresolver.sh $resolver $region ; done 
+for resolver in $reslist; do echo Restore VCN Resolver : $resolver && ./rest_vcnresolver.sh $resolver ; done 
 cd .. 
 cd ..
 
