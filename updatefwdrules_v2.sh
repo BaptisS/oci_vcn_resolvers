665c3dbd96ac7393f9b19831338c7050ad72429c
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-09-14 20:14:46 +0200

    Update updatefwdrules_v2.sh

--- updatefwdrules_v2.sh
+++ updatefwdrules_v2.sh
@@ -9,7 +9,7 @@ export res_excl3=$7
 export res_excl4=$8
 
 rm -f $res_ocid.logfile
-if [[ "$res_ocid" != "$res_excl" ] && [ "$res_ocid" != "$res_excl2" ] && [ "$res_ocid" != "$res_excl3" ] && [ "$res_ocid" != "$res_excl4" ]]; then
+if [[ "$res_ocid" != "$res_excl" ] && [ "$res_ocid" != "$res_excl2" ] && [ "$res_ocid" != "$res_excl3" ] && [ "$res_ocid" != "$res_excl4" ]] ; then
 export fw_ep_name=$(oci dns resolver get --region $res_region --resolver-id $res_ocid --query 'data.endpoints[?("is-forwarding")]' | jq -r '.[] | ."name"')
 export fw_rules='[{"action":"FORWARD","qnameCoverConditions":['$fw_domains'],"destinationAddresses":["'$fw_target'"],"sourceEndpointName":"'$fw_ep_name'"}]'
 resupdate=$(oci dns resolver update --region $res_region --resolver-id $res_ocid --rules $fw_rules --force) 
